e6f75fa8b4d044c5fc9680a8ae33587d8ffa4783
leht377 <migithub2019@gmail.com>
2022-08-05 12:50:55 -0500

    probando deploy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('./app');
 const server = http.createServer(app);
 const { PORT } = require('./utils/confing');
 const PUERT0 = PORT || 3001;
+
 server.listen(PUERT0, () => {
   console.log(`El servidor esta corriendo en el puerto ${PUERT0}`);
 });
