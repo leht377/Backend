e461c809020f420fab0dfd7773d04063bd6c421b
leht377 <migithub2019@gmail.com>
2022-08-04 22:04:08 -0500

    uibuild

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-const express = require("express")
-const app = express()
-const cors= require("cors")
-const mongoose = require("mongoose")
-const {MONGOURI} = require("./utils/confing")
-const {tokenExtractor} = require("./utils/middlewares")
-require("express-async-errors")
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const { MONGOURI } = require('./utils/confing');
+const { tokenExtractor } = require('./utils/middlewares');
+const path = require('path');
 
+require('express-async-errors');
 
+mongoose
+  .connect(MONGOURI)
+  .then(() => {
+    console.log('Mongo esta corriendo');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+app.use(tokenExtractor);
 
-mongoose.connect(MONGOURI).then(()=>{console.log("Mongo esta corriendo")}).catch((error)=>{console.log(error)})
-app.use(cors())
-app.use(express.json())
-app.use(tokenExtractor)
+const userRouter = require('./controllers/users');
+app.use('/api/user', userRouter);
 
-const userRouter = require("./controllers/users")
-app.use("/api/user", userRouter)
+const patientRouter = require('./controllers/patient');
+app.use('/api/patient', patientRouter);
 
-const patientRouter = require("./controllers/patient")
-app.use("/api/patient",patientRouter )
+const loginRouter = require('./controllers/login');
+app.use('/api/login', loginRouter);
 
-const loginRouter = require ("./controllers/login")
-app.use("/api/login", loginRouter)
-
-
-module.exports = app
-
-
-
-
-
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/build/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+module.exports = app;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const http = require ("http")
-const app = require("./app")
-const server = http.createServer(app)
-const {PORT} = require("./utils/confing")
-
-server.listen(PORT, ()=>{
-    console.log("el servidor esta corriendo en el puerto 3001")
-})
-
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
+const { PORT } = require('./utils/confing');
+const PUERT0 = PORT || 3001;
+server.listen(PUERT0, () => {
+  console.log(`El servidor esta corriendo en el puerto ${PUERT0}`);
+});
